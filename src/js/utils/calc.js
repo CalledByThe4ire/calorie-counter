f29cc40f4a8a8b93bafd1549cb5ba602ac6e75e3
Andrey Karamnoff <karamnoff@gmail.com>
2022-06-20 10:36:10 +0300

    Rewrite 'calc' util

--- src/js/utils/calc.js
+++ src/js/utils/calc.js
@@ -4,32 +4,33 @@ import {
   WeightMaintenanceSexCoefficient,
 } from '@/js/const';
 
-export const calcWeightMaintenance = (params) => {
-  const { sex, weight, height, age, coefficient } = params;
-  const formula =
-    WeightMaintenanceParamsCoefficient.WEIGHT * weight +
-    WeightMaintenanceParamsCoefficient.HEIGHT * height -
-    WeightMaintenanceParamsCoefficient.AGE * age;
+export const WeightManipulation = {
+  MAINTENANCE: (params) => {
+    const { sex, weight, height, age, coefficient } = params;
+    const formula =
+      WeightMaintenanceParamsCoefficient.WEIGHT * weight +
+      WeightMaintenanceParamsCoefficient.HEIGHT * height -
+      WeightMaintenanceParamsCoefficient.AGE * age;
 
-  switch (sex) {
-    case 'man':
-      return (formula + WeightMaintenanceSexCoefficient.MAN) * coefficient;
-    case 'woman':
-      return (formula - WeightMaintenanceSexCoefficient.WOMAN) * coefficient;
-    default:
-      throw new Error(`Unknown sex: ${sex}`);
-  }
-};
+    switch (sex) {
+      case 'man':
+        return (formula + WeightMaintenanceSexCoefficient.MAN) * coefficient;
+      case 'woman':
+        return (formula - WeightMaintenanceSexCoefficient.WOMAN) * coefficient;
+      default:
+        throw new Error(`Unknown sex: ${sex}`);
+    }
+  },
+  LOSS: (params) => {
+    const weightMaintenance = this.MAINTENANCE(params);
+    const percentage = getPercentage(15, weightMaintenance);
 
-export const calcWeightChange = (type, weightMaintenance) => {
-  const percentage = getPercentage(15, weightMaintenance);
+    return weightMaintenance - percentage;
+  },
+  GAIN: (params) => {
+    const weightMaintenance = this.MAINTENANCE(params);
+    const percentage = getPercentage(15, weightMaintenance);
 
-  switch (type) {
-    case 'loss':
-      return weightMaintenance - percentage;
-    case 'gain':
-      return weightMaintenance + percentage;
-    default:
-      throw new Error(`Unknown type: ${type}`);
-  }
+    return weightMaintenance + percentage;
+  },
 };
